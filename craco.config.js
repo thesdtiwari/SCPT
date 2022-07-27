dd94f6a82827e2ef5cca6f3993acb6710cf018f0
Saurabh Tiwari <thesdtiwari@example.com>
2022-07-27 20:25:44 +0530

    auto prefixer

--- craco.config.js
+++ craco.config.js
@@ -3,7 +3,7 @@ module.exports = {
     postcss: {
       plugins: [
         require('tailwindcss')('./src/css/tailwind.config.js'),
-        require('autoprefixer')
+        // require('autoprefixer')
       ],
     },
   },
